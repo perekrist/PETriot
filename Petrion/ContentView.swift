7287d369e4bcc8d31c3f29c37d49297035bf923d
perekrist <perekrist31@gmail.com>
2020-10-03 12:07:00 +0700

    Initial Commit

---
+++ Petrion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Petrion
+//
+//  Created by Кристина Перегудова on 03.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Petrion/PetrionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetrionApp.swift
+//  Petrion
+//
+//  Created by Кристина Перегудова on 03.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct PetrionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
